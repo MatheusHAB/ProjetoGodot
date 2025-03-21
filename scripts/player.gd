91db39cf39697a662fdfbf9812f0c5483c1f5bca
MatheusHAB <152932283+MatheusHAB@users.noreply.github.com>
2025-03-20 21:37:02 -0300

    MaisTrabai

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,19 +5,28 @@ extends CharacterBody2D
 const SPEED = 70.0
 const JUMP_VELOCITY = -240.0
 
+var jump_count = 0
+
+@export var max_jump_count = 2
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	else:
+		jump_count = 0
 		if velocity.x != 0:
 			anim.play("walk")
 		else:
 			anim.play("idle")
 
+	if is_on_wall():
+		jump_count = 0
+
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump")  and jump_count < max_jump_count:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 		anim.play("jump")
 
 	if Input.is_action_just_pressed("jump") and is_on_wall():
@@ -40,3 +49,10 @@ func _physics_process(delta: float) -> void:
 
 		
 	move_and_slide()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("DeathZone"):
+		get_tree().reload_current_scene()
+	elif area.is_in_group("LevelEnd"):
+		get_tree().change_scene_to_file("res://scenes/forest.tscn")
